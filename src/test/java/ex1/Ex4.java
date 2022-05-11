14fe94813785efac5a4390749b61ad9d13ede2f4
Ramazan Yasar <ramazanyas@gmail.com>
2022-05-11 23:49:31 +0200

    start

---
+++ src/test/java/ex1/Ex4.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class Ex4 {
+
+    /*
+    1. Yeni bir Class olusturalim.C07_ManageWindowSet
+    2. Amazon soyfasina gidelim. https://www.amazon.com/
+    3. Sayfanin konumunu ve boyutlarini yazdirin
+    4. Sayfanin konumunu ve boyutunu istediginiz sekilde ayarlayin
+    5. Sayfanin sizin istediginiz konum ve boyuta geldigini test edin
+    8. Sayfayi kapatin
+     */
+
+}
